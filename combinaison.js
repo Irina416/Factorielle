8fdbf76a59d29a8b9f197720e642b878f5294012
Irina <irinarakoto2005@gmail.com>
2025-03-10 20:22:09 +0300

    Finalisation task

--- combinaison.js
+++ combinaison.js
@@ -1,7 +1,14 @@
-function combinaison(n, p){
+// Syntaxe importation d'un export default est sans {...}
+// Ici c'est avec car => export function factorielle(nombre) {...}
+import { factorielle } from "./factorielle.js";
+
+function combinaison(n, p) {
     if (n > p) {
-        return factorielle(n) / factorielle (n-p) * factorielle(p);
+        return factorielle(n) / (factorielle(p) * factorielle(n - p));
     } else {
+        // Crée notre propre message d'erreur si n < p
         throw new Error('n must be greater than p')
     }
 }
+
+console.log(combinaison(5, 3));  // 10
